c20263c6149f65cd0877457fab8793c296f6b306
lth1503ahihi <nguyenvictor2000o@gmail.com>
2026-06-16 10:12:30 +0700

    Not change fonts

--- bin/modfile/UpdateFile/Fonts/update.sh
+++ bin/modfile/UpdateFile/Fonts/update.sh
@@ -7,49 +7,6 @@ rom_os=$(cat $work_dir/bin/ddevice/rom_os.txt)
 deviceTYPE=$(cat $work_dir/bin/ddevice/device_type.txt)
 MAIN_FOLDER="$work_dir/build/baserom/images"
 
-
-mods "Fix Fonts"
-if [[ $deviceTYPE == "China" ]];then
-    if [[ $rom_os == "MIUI" ]];then
-        mods "Detect MIUI!Adding..."
-        cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/MIUI/fonts.xml $work_dir/build/baserom/images/system/system/etc/
-        cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/MIUI/*.ttf $work_dir/build/baserom/images/system/system/fonts/
-    elif [[ $rom_os == "OS1" ]] && [[ $androidVer -le "13" ]];then
-        mods "Detect HyperOS A13!Adding..."
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansLatinVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF_Overlay.ttf
-        cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/*.ttf $work_dir/build/baserom/images/system/system/fonts/
-	elif [[ $rom_os == "OS1" ]] && [[ $androidVer -le "14" ]];then
-        mods "Detect HyperOS!Adding..."
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansLatinVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF_Overlay.ttf
-        cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/*.ttf $work_dir/build/baserom/images/system/system/fonts/
-		cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/A14/*.ttf $work_dir/build/baserom/images/product/fonts/
-	elif [[ $rom_os == "OS2" ]] && [[ $androidVer -le "14" ]];then
-        mods "Detect HyperOS!Adding..."
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansLatinVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF_Overlay.ttf
-        cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/*.ttf $work_dir/build/baserom/images/system/system/fonts/
-		cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/A14/*.ttf $work_dir/build/baserom/images/product/fonts/
-	elif [[ $rom_os == "OS2" ]] && [[ $androidVer -le "15" ]];then
-        mods "Detect HyperOS!Adding..."
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansLatinVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF_Overlay.ttf
-        cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/*.ttf $work_dir/build/baserom/images/system/system/fonts/
-		cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/A14/*.ttf $work_dir/build/baserom/images/product/fonts/
-	elif [[ $rom_os == "OS3" ]] && [[ $androidVer -le "16" ]];then
-        mods "Detect HyperOS!Adding..."
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansLatinVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF.ttf
-		rm $work_dir/build/baserom/images/system/system/fonts/MiSansVF_Overlay.ttf
-        cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/*.ttf $work_dir/build/baserom/images/system/system/fonts/
-		cp -rf $work_dir/bin/modfile/UpdateFile/Fonts/HyperOS/A14/*.ttf $work_dir/build/baserom/images/product/fonts/
-    fi
-else
-mods "Global ROM!No Adding..."
-fi
+mods "Bypass Fix Fonts - Keep Original Fonts"
+# Toàn bộ logic ép đổi font đã được xóa bỏ để giữ nguyên font gốc siêu mượt của nhà sản xuất
 mods "Done"
