26d8e5a887c2e9fb7e9040051c6e1701981e9ca5
lth1503ahihi <nguyenvictor2000o@gmail.com>
2026-07-29 21:35:57 +0700

    fix ggdrive

--- uploadROM.sh
+++ uploadROM.sh
@@ -75,9 +75,14 @@ echo "${os_type}_${polyxver}_${device_code}_${base_rom_code}_${hash}_${status}.z
 # Đặt tên thư mục upload trên Drive trùng với hệ điều hành
 uploaddir=$true_os
 
-# Upload thẳng lên Google Drive (Tốc độ bình thường, không giới hạn)
+# Upload thẳng lên Google Drive (Đã tối ưu rclone chống kẹt 100%)
 upload "Uploading to Google Drive..."
-rclone -v --config="$RCLONE_CONFIG_1DRIVE" copy "$output_file" "$GDRIVE_REMOTE:$GDRIVE_FOLDER/${uploaddir}/${polyxver}/${device_code}/" || {
+rclone -v --config="$RCLONE_CONFIG_1DRIVE" copy "$output_file" "$GDRIVE_REMOTE:$GDRIVE_FOLDER/${uploaddir}/${polyxver}/${device_code}/" \
+    --drive-chunk-size 128M \
+    --tpslimit 4 \
+    --retries 3 \
+    --timeout 15m \
+    --contimeout 15m || {
     upload "Lỗi khi upload file lên Google Drive!"
     exit 1
 }
