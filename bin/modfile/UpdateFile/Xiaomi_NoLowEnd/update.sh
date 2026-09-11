3fedcffaad4ee83b9eb888b7e274818b16dfa273
lth1503ahihi <nguyenvictor2000o@gmail.com>
2026-09-12 03:34:52 +0700

    Add product overlay

--- bin/modfile/UpdateFile/Xiaomi_NoLowEnd/update.sh
+++ bin/modfile/UpdateFile/Xiaomi_NoLowEnd/update.sh
@@ -1,18 +1,15 @@
-
 work_dir=$(pwd)
-source "$work_dir/functions.sh"
+source $work_dir/functions.sh
 
-find . -type f -name "*.prop" | while read -r f; do
-    sed -i \
-        -e 's/^ro.config.low_ram.threshold_gb\s*=.*/ro.config.low_ram.threshold_gb=0/' \
-        -e 's/^ro.config.low_ram.middle.threshold_gb\s*=.*/ro.config.low_ram.middle.threshold_gb=0/' \
-        -e 's/^persist.sys.computility.gpulevel\s*=.*/persist.sys.computility.gpulevel=2/' \
-        -e 's/^persist.sys.computility.cpulevel\s*=.*/persist.sys.computility.cpulevel=2/' \
-        -e 's/^vendor.perf.framepacing.enable\s*=.*/vendor.perf.framepacing.enable=true/' \
-        -e 's/^persist.sys.sf_charge_anim_supported\s*=.*/persist.sys.sf_charge_anim_supported=true/' \
-        -e 's/^ro.miui.has_handy_mode_sf\s*=.*/ro.miui.has_handy_mode_sf=1/' \
-        -e 's/^persist.sys.advanced_visual_release\s*=.*/persist.sys.advanced_visual_release=3/' \
-        -e 's/^persist.sys.power.default.powermode\s*=.*/persist.sys.power.default.powermode=0/' \
-        -e 's/^ro.miui.support_miui_ime_bottom\s*=.*/ro.miui.support_miui_ime_bottom=1/' \
-        "$f"
+for f in $(find . -type f -name "*.prop"); do
+    sed -i 's/^ro.config.low_ram.threshold_gb\s*=.*/ro.config.low_ram.threshold_gb=0/' "$f"
+    sed -i 's/^ro.config.low_ram.middle.threshold_gb\s*=.*/ro.config.low_ram.middle.threshold_gb=0/' "$f"
+    sed -i 's/^persist.sys.computility.gpulevel\s*=.*/persist.sys.computility.gpulevel=4/' "$f"
+    sed -i 's/^persist.sys.computility.cpulevel\s*=.*/persist.sys.computility.cpulevel=4/' "$f"
+    sed -i 's/^vendor.perf.framepacing.enable\s*=.*/vendor.perf.framepacing.enable=true/' "$f"
+    sed -i 's/^persist.sys.sf_charge_anim_supported\s*=.*/persist.sys.sf_charge_anim_supported=true/' "$f"
+    sed -i 's/^ro.miui.has_handy_mode_sf\s*=.*/ro.miui.has_handy_mode_sf=1/' "$f"
+    sed -i 's/^persist.sys.advanced_visual_release\s*=.*/persist.sys.advanced_visual_release=3/' "$f"
+    sed -i 's/^persist.sys.power.default.powermode\s*=.*/persist.sys.power.default.powermode=1/' "$f"
+    sed -i 's/^ro.miui.support_miui_ime_bottom\s*=.*/ro.miui.support_miui_ime_bottom=1/' "$f"
 done
